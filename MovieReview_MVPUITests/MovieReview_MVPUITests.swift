3dc6d3400fcf912aeec5761b2768b4b9a8ad4c63
soduma <soduma@naver.com>
2022-01-26 13:51:44 +0900

    UITest

--- MovieReview_MVPUITests/MovieReview_MVPUITests.swift
+++ MovieReview_MVPUITests/MovieReview_MVPUITests.swift
@@ -8,37 +8,46 @@
 import XCTest
 
 class MovieReviewMVPUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state
-        // - such as interface orientation - required for your tests before they run.
-        // The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false // 하나만 실패해도 계속 진행되니까 멈추도록 함
+        app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        app = nil
+    }
+    
+    func test_navigationBar의title이영화평점으로설정되어있다() {
+        let existNavigationBar = app.navigationBars["영화 평점"]
+            .exists
+        XCTAssertTrue(existNavigationBar)
+    }
+    
+    func test_searchBar가존재한다() {
+        let existSearchBar = app.navigationBars["영화 평점"]
+            .searchFields["Search"] // placeholder
+            .exists
+        
+        XCTAssertTrue(existSearchBar)
+    }
+    
+    func test_searchBar에cancel버튼이존재한다() {
+        let navigationBar = app.navigationBars["영화 평점"]
+        navigationBar
+            .searchFields["Search"]
+            .tap()
+        
+        let existSearchBarCancelButton = navigationBar
+            .buttons["Cancel"]
+            .exists
+        
+        XCTAssertTrue(existSearchBarCancelButton)
     }
 }
